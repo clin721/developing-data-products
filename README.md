992c42ef2560b4f940888f94981ac8ee392520de
Lin <clin1002@LAMU02QT4VRG8WP.uhc.com>
2017-08-18 16:22:07 -0400

    uploading project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# developing-data-products
+# Course 9 - Developing Data Products
+
+Descriptive Statistics for a Sample Population
+
+This application lets you create a normal distribution with 100-200 samples in it and set the seed used by R from 1-2000. Once those parameters are set, it plots a histogram showing the distribution of points along with some descriptive statistics for the population like the mean, standard deviation, and standard error of the mean.
---
+++ simple_stats/server.R
@@ -0,0 +1,39 @@
+
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  #read in number from slider bar and create a normal distribution
+  samp <- reactive({ 
+    set.seed(as.numeric(input$slider2))
+    rnorm(as.numeric(input$slider1))
+  })
+  
+  output$mean <- reactive({ ifelse(input$show_mean, mean(samp()),"")})
+  output$stddev <- reactive({ifelse(input$show_stddev, sd(samp()),"")})
+  output$stderr <- reactive({
+    if(input$show_stderr) 
+      sd(samp())/sqrt(as.numeric(input$slider1))
+    else
+      ""
+  })
+  
+  output$confint <- reactive({
+    if(input$show_confint){
+      x = samp()
+      mean = mean(x)
+      std = sd(x)
+      err = qnorm(.975)*std/sqrt(as.numeric(input$slider1))
+      round(c(mean-err,mean+err),3)
+    } else
+      ""
+  })
+  
+  # draw the histogram for the sample
+  output$distPlot <- renderPlot({
+    
+    hist(samp(), col = 'darkgray', border = 'white')
+  })
+  
+})
